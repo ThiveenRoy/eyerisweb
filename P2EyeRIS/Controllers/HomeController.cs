d49d825557731a36f7053e2d070d3ce15fb86f13
Zheng Wee Lee <s10185455@connect.np.edu.sg>
2020-01-07 12:13:03 +0800

    student login links to chart cshtml

--- P2EyeRIS/Controllers/HomeController.cs
+++ P2EyeRIS/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace P2EyeRIS.Controllers
     {
         public IActionResult Index()
         {
-            return View();  
+            return View();
             //insert login verification
         }
         public IActionResult studentLogin()
@@ -40,7 +40,30 @@ namespace P2EyeRIS.Controllers
                 return RedirectToAction("Index");
             }
         }
+        [HttpPost]
+        public ActionResult StudentLogin(IFormCollection formData)
+        {
+            // Read inputs from textboxes
+            // username converted to lowercase
+            string loginID = formData["username"].ToString().ToLower();
+            string password = formData["password"].ToString();
 
+            if (loginID == "user" && password == "password")
+            {                 // Redirect user to the "LecturerMain" view through an action
+                return RedirectToAction("Chart");
+            }
+            //testing>>>>>
+            else if (loginID == "no" && password == "no")
+            {
+                return RedirectToAction("TestChart");
+            }
+            else
+            {
+                // Redirect user back to the index view through an action
+
+                return RedirectToAction("Index");
+            }
+        }
         public ActionResult LecturerMain()
         {
             return View();
@@ -56,40 +79,5 @@ namespace P2EyeRIS.Controllers
         {
             return View();
         }
-        public async Task<ActionResult> About()
-        {
-            //Simulate test user data and login timestamp
-            var userId = "12345";
-            var currentLoginTime = DateTime.UtcNow.ToString("MM/dd/yyyy HH:mm:ss");
-
-            //Save non identifying data to Firebase
-            var currentUserLogin = new LoginData() { TimestampUtc = currentLoginTime };
-            var firebaseClient = new FirebaseClient("https://eeyes-68b9c.firebaseio.com");
-            var result = await firebaseClient
-              .Child("Users/" + userId + "/Logins")
-              .PostAsync(currentUserLogin);
-
-            //Retrieve data from Firebase
-            var dbLogins = await firebaseClient
-              .Child("Users")
-              .Child(userId)
-              .Child("Logins")
-              .OnceAsync<LoginData>();
-
-            var timestampList = new List<DateTime>();
-
-            //Convert JSON data to original datatype
-            foreach (var login in dbLogins)
-            {
-                timestampList.Add(Convert.ToDateTime(login.Object.TimestampUtc).ToLocalTime());
-            }
-
-            //Pass data to the view
-            ViewBag.CurrentUser = userId;
-            ViewBag.Logins = timestampList.OrderByDescending(x => x);
-            return View();
-        }
-
-
     }
 }
