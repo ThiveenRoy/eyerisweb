33e8fc289096192fca58f3dd1f7140b07c5f3014
Thiveen Roy <thiveen@gmail.com>
2020-01-13 11:47:03 +0800

    lol

---
+++ P2EyeRIS/wwwroot/js/index.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+
+    
+    console.log("hello");
+
+    $(".register form").on("submit", function (event) {
+        event.preventDefault();
+
+        var email = $(".register .email").val();
+        var password = $(".register .password").val();
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(function (user) {
+                console.log(user);
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+        
+        
+
+
+    })
+
+})
